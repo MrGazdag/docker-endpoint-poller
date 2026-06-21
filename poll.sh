b2fda39b2991ee33b8faa2c7c2b9edac66ef6971
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2026-06-21 22:16:55 +0200

    merge default options

--- poll.sh
+++ poll.sh
@@ -2,11 +2,11 @@
 
 URL="${URL:-https://httpbin.org/uuid}"
 INTERVAL="${INTERVAL:-5}"
-CURL_OPTIONS="${CURL_OPTIONS:-s}"
+CURL_OPTIONS="${CURL_OPTIONS:--s --show-error}"
 
 while true; do
   echo "Requesting: $URL"
-  curl $CURL_OPTIONS "$URL" --show-error
+  curl $CURL_OPTIONS "$URL"
   echo
   sleep "$INTERVAL"
 done
