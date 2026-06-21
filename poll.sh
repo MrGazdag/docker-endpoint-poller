51fa5a295249f5c38095aa8408f62166d2684155
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2026-06-21 22:06:59 +0200

    add curl options

--- poll.sh
+++ poll.sh
@@ -2,10 +2,11 @@
 
 URL="${URL:-https://httpbin.org/uuid}"
 INTERVAL="${INTERVAL:-5}"
+CURL_OPTIONS="${CURL_OPTIONS:-s}"
 
 while true; do
   echo "Requesting: $URL"
-  curl -s "$URL" --show-error
+  curl $CURL_OPTIONS "$URL" --show-error
   echo
   sleep "$INTERVAL"
 done
